58d269d8a4ea5927e701d42e8dd846727d2c08aa
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2022-09-14 17:30:13 -0500

    feat create routes

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+/* navbar component */
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <h1>Navbar</h1>
+      <image />
+      <p>logo to profile</p>
+    </div>
+  );
+}
+
+export default Navbar;
